20a46a2ac4045d5e3e60d35242c176fd7946cf95
Tim Herzig <tim.herzig@hotmail.com>
2022-10-27 15:17:47 +0200

    changes to run on cluster

--- mnist_1/emnest_train.py
+++ mnist_1/emnest_train.py
@@ -8,12 +8,16 @@ from pl_bolts.datasets import MNIST
 from torch.utils.data import DataLoader, random_split
 from emnest_autoencoder import Emnest_AutoEncoder
 
+from lib.parser import parse_arguments
+
 pl.seed_everything(1234)
 batch_size = 4096
 
-dataset = MNIST(os.getcwd(), train=True, download=True, transform=transforms.ToTensor())
+args = parse_arguments()
 
-mnist_test = MNIST(os.getcwd(), train=False, download=True, transform=transforms.ToTensor())
+dataset = MNIST(args.mnist, train=True, download=False, transform=transforms.ToTensor())
+
+mnist_test = MNIST(args.mnist, train=False, download=False, transform=transforms.ToTensor())
 
 mnist_train, mnist_val = random_split(dataset, [55000, 5000])
 
@@ -24,9 +28,12 @@ val_loader = DataLoader(mnist_val, batch_size=batch_size)
 
 model = Emnest_AutoEncoder(batch_size=batch_size, learning_rate=1e-3)
 
-trainer = pl.Trainer(accelerator='gpu', devices=[1], max_epochs=30)
+trainer = pl.Trainer(accelerator='gpu', max_epochs=2, default_root_dir=args.checkpoint)
 
 print(f'Number of devices: {torch.cuda.device_count()}')
 print(f'Device used: {torch.cuda.get_device_name(torch.cuda.current_device())}')
 
 trainer.fit(model, train_loader, val_loader)
+
+print('Finished training')
+print(f'Checkpoint location {args.checkpoint}')
