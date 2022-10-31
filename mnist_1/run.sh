b53a354aff6c3e4c29dfdea09738d5c958d44aa2
Tim Herzig <tim.herzig@hotmail.com>
2022-10-31 11:33:30 +0100

    container changes

--- mnist_1/run.sh
+++ mnist_1/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 srun \
---container-image=/netscratch/enroot/dlcc_pytorch_20.10.sqsh \ #/netscratch/enroot/nvcr.io_nvidia_pytorch_22.10-py3.sqsh\
+--container-image=/netscratch/enroot/dlcc_pytorch_20.10.sqsh \
 --container-mounts=/ds:/ds,/netscratch:/netscratch,"`pwd`":"`pwd`" \
 --container-workdir="`pwd`" \
 install.sh python3 emnest_train.py -mnist '/ds/images/MNIST' -checkpoint '/netscratch/herzig/mnist_1'
