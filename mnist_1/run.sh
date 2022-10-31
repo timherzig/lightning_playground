8948c410e7dd2633a046973dc14857eb9a4a69c4
Tim Herzig <tim.herzig@hotmail.com>
2022-10-31 11:38:05 +0100

    container changes

--- mnist_1/run.sh
+++ mnist_1/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 srun \
---container-image=/netscratch/enroot/hufe_slt_dlcc_pytorch_20.10.sqsh.packages:pytorch-lightning==1.0.4 \
+--container-image=/netscratch/enroot/hufe_slt_dlcc_pytorch_20.10.sqsh \
 --container-mounts=/ds:/ds,/netscratch:/netscratch,"`pwd`":"`pwd`" \
 --container-workdir="`pwd`" \
 install.sh python3 emnest_train.py -mnist '/ds/images/MNIST' -checkpoint '/netscratch/herzig/mnist_1'
