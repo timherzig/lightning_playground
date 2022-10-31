0f5c42f906c839950df592d3637a03fb64dcaeb4
Tim Herzig <tim.herzig@hotmail.com>
2022-10-31 09:53:15 +0100

    run script changes

--- mnist_1/run.sh
+++ mnist_1/run.sh
@@ -2,7 +2,8 @@
 
 srun \
 --container-image=/netscratch/enroot/dlcc_pytorch_20.10.sqsh.packages:pytorch-lightning==1.0.4 \
---container-mounts=/home/$USER/misc/lightning_playground/mnist_1:/src/mnist \
+--container-workdir=/home/$USER/misc/lightning_playground/mnist_1:/src \
 --container-mounts=/ds/images/MNIST:/ds/MNIST \
---container-workdir=/netscratch/$USER/mnist_1:/cpt/mnist \
-python3 /src/mnist/emnest_train.py -mnist /ds/MNIST -checkpoint /cpt/mnist
+--container-mounts=/netscratch/$USER/mnist_1:/cpt/mnist \
+python3 /src/emnest_train.py -mnist /ds/MNIST -checkpoint /cpt/mnist
+echo 'running'
