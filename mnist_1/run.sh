dc08f9dcad282c02192e0aa4785be6b4d3e91c59
Tim Herzig <tim.herzig@hotmail.com>
2022-10-31 11:54:17 +0100

    container changes

--- mnist_1/run.sh
+++ mnist_1/run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 srun \
+--gpus=1 \
 --container-image=/netscratch/enroot/nvcr.io_nvidia_pytorch_22.10-py3.sqsh \
 --container-mounts=/ds:/ds,/netscratch:/netscratch,"`pwd`":"`pwd`" \
 --container-workdir="`pwd`" \
