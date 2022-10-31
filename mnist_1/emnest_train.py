100ae47fab4ed1b6bd2af42645d558aa4ae870eb
Tim Herzig <tim.herzig@hotmail.com>
2022-10-31 10:27:06 +0100

    prints in train-file

--- mnist_1/emnest_train.py
+++ mnist_1/emnest_train.py
@@ -16,7 +16,7 @@ batch_size = 4096
 args = parse_arguments()
 
 print(f'Getting MNIST from: {args.mnist}')
-print(f'Checkpoint location: {args.ccheckpoint}')
+print(f'Checkpoint location: {args.checkpoint}')
 
 dataset = MNIST(args.mnist, train=True, download=False, transform=transforms.ToTensor())
 
