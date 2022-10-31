5f2ac40f02a8cc27d02a45bbc3db20c90092c8a0
Tim Herzig <tim.herzig@hotmail.com>
2022-10-31 10:37:07 +0100

    run script changes

--- mnist_1/run.sh
+++ mnist_1/run.sh
@@ -2,9 +2,7 @@
 
 srun \
 --container-image=/netscratch/enroot/dlcc_pytorch_20.10.sqsh \
---container-mounts=/ds/images/MNIST:/ds/MNIST \
---container-mounts=/netscratch/$USER/mnist_1:/cpt/mnist \
---container-mounts=/home/herzig/misc/lightning_playground/mnist_1:/src \
---container-workdir=/src \
+--container-mounts=/ds/images/MNIST:/ds/MNIST,/netscratch/$USER/mnist_1:/cpt/mnist \
+--container-workdir="`pwd`" \
 install.sh python3 emnest_train.py -mnist '/ds/MNIST' -checkpoint '/cpt/mnist'
 echo 'running'
